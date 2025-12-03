6cd18583bf1dbdeb33468f44477bec23ce67e756
KostikovE <uitro355@gmail.com>
2025-12-03 22:55:50 +0300

    Create config-sample.php

---
+++ lab8/config-sample.php
@@ -0,0 +1,8 @@
+<?php
+
+define('DB_NAME', 'ваша_база_данных');
+define('DB_USER', 'ваш_пользователь');
+define('DB_PASSWORD', 'ваш_пароль');
+define('DB_HOST', 'localhost');
+define('DB_CHARSET', 'utf8mb4');
+?>
