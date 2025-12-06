139dc1a4e6d85ae496c3957cd2ef1cbd13d8cbfe
KostikovE <uitro355@gmail.com>
2025-12-06 09:35:32 +0300

    Update User.php

--- lab7/MyProject/Classes/User.php
+++ lab7/MyProject/Classes/User.php
@@ -3,14 +3,36 @@ namespace MyProject\Classes;
 
 class User
 {
-  
+    /**
+     * Имя пользователя
+     * @var string $name Полное имя пользователя
+     * @access public
+     */
     public $name;
+    
+    /**
+     * Логин пользователя
+     * @var string $login Логин пользователя
+     * @access public
+     */
     public $login;
     
-  
+    /**
+     * Пароль пользователя
+     * @var string $password Пароль пользователя
+     * @access private
+     */
     private $password;
     
-   
+    /**
+     * Конструктор класса User
+     * @param string $name Полное имя пользователя
+     * @param string $login Логин пользователя 
+     * @param string $password Пароль пользователя
+     * 
+     * @return void
+     * @access public
+     */
     public function __construct($name, $login, $password)
     {
         $this->name = $name;
@@ -18,13 +40,26 @@ class User
         $this->password = $password;
     }
     
- 
+    /**
+     * Деструктор класса User
+     * @return void
+     * @access public
+     */
     public function __destruct()
     {
         echo "Пользователь {$this->login} удален.<br>";
     }
     
- 
+    /**
+     * Метод для отображения информации о пользователе
+     * @return void
+     * @access public
+     * 
+     * @example
+     * $user = new User("Иван Иванов", "ivanov", "secret123");
+     * $user->showInfo();
+    
+     */
     public function showInfo()
     {
         echo "--- Информация о пользователе ---<br>";
